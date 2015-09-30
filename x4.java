401bfbee2d898a22cb27d058be5dbf30f130c9bf
clarc54 <chris20topher@yahoo.com>
2015-09-30 14:09:48 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -9,40 +9,42 @@ void setup() {
 void draw() {
   background(0, 255, 0);
   fill(0);
-  text("System Operational", 350, 250);
-  text("Ready For Use", 365, 265);
+  textSize(15);
+  text("System Operational", 335, 255);
+  text("Ready For Use", 355, 270);
   //Button
   fill(255, 0, 0);
+  strokeWeight(4);
   rect(350, 193, 105, 45);
   fill(0);
-  text("Do Not Press", 368, 222);
+  text("Do Not Press", 356, 222);
 }
 
 //Handlers
 void keyPressed() {
   if (key == 'q') exit();
 }
-void mouseClicked() {
- if (rect(350, 193, 105, 45));
- }
- else 
- (background(255,0,0);
-  fill(0);
-  text("System Malfuntion", 350, 250);
-  text("Error Error Error", 365, 265);
+//void mouseClicked() {
+ //if (rect(350, 193, 105, 45));
+ //}
+ //else
+ //(background(255,0,0);
+ // fill(0);
+  //text("System Malfuntion", 350, 250);
+  //text("Error Error Error", 365, 265);
   //Button
-  fill(0, 255, 0);
-  rect(350, 193, 105, 45);
-  fill(0);
-  text("I told you not to press the button...", 368, 222); ) {
-  background(255,0,0);
-  fill(0);
-  text("System Malfuntion", 350, 250);
-  text("Error Error Error", 365, 265);
+  //fill(0, 255, 0);
+  //rect(350, 193, 105, 45);
+  //fill(0);
+  //text("I told you not to press the button...", 368, 222); ) {
+  //background(255,0,0);
+  //fill(0);
+  //text("System Malfuntion", 350, 250);
+  //text("Error Error Error", 365, 265);
   //Button
-  fill(0, 255, 0);
-  rect(350, 193, 105, 45);
-  fill(0);
-  text("I told you not to press the button...", 368, 222);)
+  //fill(0, 255, 0);
+  //rect(350, 193, 105, 45);
+  //fill(0);
+  //text("I told you not to press the button...", 368, 222);)
   
-}
+//}
