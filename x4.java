44ea81f7152d60a8d2b18e2ec60109938a9b6401
clarc54 <chris20topher@yahoo.com>
2015-10-02 13:06:49 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -5,8 +5,15 @@
 void setup() {
   size(640, 480);
 }
-//Background
+
+//Setup Scenes
 void draw() {
+  scene1 ();
+  scene2 ();
+}
+
+//Draw Scenes
+void scene1 () {
   background(0, 255, 0);
   fill(0);
   textSize(15);
@@ -20,26 +27,27 @@ void draw() {
   text("Do Not Press", 356, 222);
 }
 
+void scene2 () {
+ mouseClicked() {
+ if (rect(350, 193, 105, 45));
+ }
+ else
+ (background(255,0,0);
+ fill(0);
+  text("System Malfuntion", 350, 250);
+  text("Error Error Error", 365, 265);
+  //Button
+  fill(0, 255, 0);
+  rect(350, 193, 105, 45);
+  fill(0);
+  text("I told you not to press the button...", 368, 222); ) {
+  background(255,0,0);
+  fill(0);
+  text("System Malfuntion", 350, 250);
+  text("Error Error Error", 365, 265);
+  }
+  
 //Handlers
 void keyPressed() {
   if (key == 'q') exit();
 }
-//void mouseClicked() {
- //if (rect(350, 193, 105, 45));
- //}
- //else
- //(background(255,0,0);
- // fill(0);
-  //text("System Malfuntion", 350, 250);
-  //text("Error Error Error", 365, 265);
-  //Button
-  //fill(0, 255, 0);
-  //rect(350, 193, 105, 45);
-  //fill(0);
-  //text("I told you not to press the button...", 368, 222); ) {
-  //background(255,0,0);
-  //fill(0);
-  //text("System Malfuntion", 350, 250);
-  //text("Error Error Error", 365, 265);
-  
-//}
