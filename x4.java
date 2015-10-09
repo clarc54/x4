1c0d8d56292ec6834f266c2f800aaa5d389f3afb
clarc54 <chris20topher@yahoo.com>
2015-10-09 13:00:00 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -41,27 +41,27 @@ void scene1 () {
   text("Do Not Press", 356, 222);
 }
 
-/*
+
 void scene2 () {
- mouseClicked() {
+ mouseClicked()
  if (rect(350, 193, 105, 45));
+ else (background(255,0,0);
  }
- else
- (background(255,0,0);
  fill(0);
+ textSize(15);
   text("System Malfuntion", 350, 250);
   text("Error Error Error", 365, 265);
   //Button
   fill(0, 255, 0);
+  strokeWeight(4);
   rect(350, 193, 105, 45);
   fill(0);
-  text("I told you not to press the button...", 368, 222); ) {
-  background(255,0,0);
-  fill(0);
+  text("I told you not to press the button...", 368, 222);
   text("System Malfuntion", 350, 250);
   text("Error Error Error", 365, 265);
   }
- */
+ 
+
 //Handlers
 void keyPressed() {
   if (key == 'q') exit();
