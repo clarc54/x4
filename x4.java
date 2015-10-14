f8c83b72bfc88f02ff2680acbca36451f30f43d1
clarc54 <chris20topher@yahoo.com>
2015-10-14 14:04:10 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -10,7 +10,7 @@ void setup() {
 //Setup Scenes
 void draw() {
   scene1 ();
-  //scene2 ();
+  mouseClicked ();
 }
 
 void reset () {
@@ -41,24 +41,20 @@ void scene1 () {
   text("Do Not Press", 356, 222);
 }
 
-
-void scene2 () {
-  //boolean a=true;
-    if (mousePressed(mouseX>=350 & mouseX<=455));
-    else {
+void mouseClicked() {
+  if (mouseX>=350 & mouseX<=455 & mouseY>=193 & mouseY<=238) {
     background(255,0,0);
     fill(0);
     textSize(15);
-    text("System Malfuntion", 350, 250);
-    text("Error Error Error", 365, 265);
+    text("System Malfuntion", 337, 255);
+    text("Error Error Error", 346, 270);
     //Button
     fill(0, 255, 0);
     strokeWeight(4);
     rect(350, 193, 105, 45);
     fill(0);
-    text("I told you not to press the button...", 368, 222);
-    text("System Malfuntion", 350, 250);
-    text("Error Error Error", 365, 265);
+    text("I told you not to press the button...", 285, 185);
+    text("Pressed", 375, 222);
   }
 }
 
