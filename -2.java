19aa67fda4ed39e2d56c27d7e4fccc17cf5b7367
clarc54 <chris20topher@yahoo.com>
2015-10-16 12:40:24 -0400

    Create -2.java

---
+++ -2.java
@@ -0,0 +1,92 @@
+//Christopher Clark - Button Background Color change project
+//Sept 30, 2015
+
+//Setup
+void setup() {
+  size(640, 480);
+  reset();
+}
+
+//Setup Scenes
+void draw() {
+  if (mousePressed == true) {
+    mouseX>=350 & mouseX<=455 & mouseY>=193 & mouseY<=238)
+    background(255,0,0);
+    fill(0);
+    textSize(15);
+    text("System Malfuntion", 337, 255);
+    text("Error Error Error", 346, 270);
+    //Button
+    fill(0, 255, 0);
+    strokeWeight(4);
+    rect(350, 193, 105, 45);
+    fill(0);
+    text("I told you not to press the button...", 285, 185);
+    text("Pressed", 375, 222);
+  }
+  if (mousePressed == false) {
+  background(0, 255, 0);
+  fill(0);
+  textSize(15);
+  text("System Operational", 335, 255);
+  text("Ready For Use", 355, 270);
+  //Button
+  fill(255, 0, 0);
+  strokeWeight(4);
+  rect(350, 193, 105, 45);
+  fill(0);
+  text("Do Not Press", 356, 222);
+  }
+}
+
+void reset () {
+  background(0, 255, 0);
+  fill(0);
+  textSize(15);
+  text("System Operational", 335, 255);
+  text("Ready For Use", 355, 270);
+  //Button
+  fill(255, 0, 0);
+  strokeWeight(4);
+  rect(350, 193, 105, 45);
+  fill(0);
+  text("Do Not Press", 356, 222);
+}
+//Draw Scenes
+/*void scene1 () {
+  background(0, 255, 0);
+  fill(0);
+  textSize(15);
+  text("System Operational", 335, 255);
+  text("Ready For Use", 355, 270);
+  //Button
+  fill(255, 0, 0);
+  strokeWeight(4);
+  rect(350, 193, 105, 45);
+  fill(0);
+  text("Do Not Press", 356, 222);
+}
+
+  void mousePressed {
+  if (mouseX>=350 & mouseX<=455 & mouseY>=193 & mouseY<=238) {
+    background(255,0,0);
+    fill(0);
+    textSize(15);
+    text("System Malfuntion", 337, 255);
+    text("Error Error Error", 346, 270);
+    //Button
+    fill(0, 255, 0);
+    strokeWeight(4);
+    rect(350, 193, 105, 45);
+    fill(0);
+    text("I told you not to press the button...", 285, 185);
+    text("Pressed", 375, 222);
+  }
+}
+*/
+
+//Handlers
+void keyPressed() {
+  if (key == 'q') exit();
+  if (key == 'r') reset();
+}
